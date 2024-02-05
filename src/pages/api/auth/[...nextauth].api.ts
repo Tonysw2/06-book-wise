c04ed59bcdf10b715462ad837ffc85d79b1c741f
Tonysw2 <anthonyrmayaa@gmail.com>
2024-02-05 13:26:02 -0300

    feat: validate env variables with zod

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -3,6 +3,7 @@ import NextAuth, { NextAuthOptions } from 'next-auth'
 import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google'
 import GithubProvider, { GithubProfile } from 'next-auth/providers/github'
 import { NextApiRequest, NextApiResponse } from 'next'
+import { env } from '@/env'
 
 export function buildNextAuthOptions(
   req: NextApiRequest,
@@ -11,11 +12,10 @@ export function buildNextAuthOptions(
   return {
     adapter: PrismaAdapter(),
 
-    // Configure one or smore authentication providers
     providers: [
       GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+        clientId: env.GOOGLE_CLIENT_ID,
+        clientSecret: env.GOOGLE_CLIENT_SECRET,
         profile(profile: GoogleProfile) {
           return {
             id: profile.sub,
@@ -27,8 +27,8 @@ export function buildNextAuthOptions(
       }),
 
       GithubProvider({
-        clientId: process.env.GITHUB_CLIENT_ID ?? '',
-        clientSecret: process.env.GITHUB_CLIENT_SECRET ?? '',
+        clientId: env.GITHUB_CLIENT_ID,
+        clientSecret: env.GITHUB_CLIENT_SECRET,
         profile(profile: GithubProfile) {
           return {
             id: profile.id,
@@ -38,8 +38,6 @@ export function buildNextAuthOptions(
           }
         },
       }),
-
-      // ...add more providers here
     ],
 
     callbacks: {
